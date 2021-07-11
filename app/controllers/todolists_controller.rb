558c81f84899fca93215be8b9e9b220f95c06c3d
umiiii419 <umiiii419@gmail.com>
2021-07-11 05:56:15 +0000

    [Add]2-5詳細画面作成 showアクション追加

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -13,6 +13,10 @@ class TodolistsController < ApplicationController
     @lists = List.all
   end
 
+  def show
+    @list = List.find(params[:id])
+  end
+
   private
   def list_params
     params.require(:list).permit(:title, :body)
